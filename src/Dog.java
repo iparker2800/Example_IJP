74f6045e18855a63829bc9eef03c794f6ba903dc
Isaiah J. Parker <iparker@bellarmine.edu>
2020-01-08 09:44:08 -0500

    initial commit

---
+++ src/Dog.java
@@ -0,0 +1,60 @@
+/**
+ * This class models the Dog 
+ * @author iparker
+ *
+ */
+public class Dog {
+
+ private String name;
+ private int age;
+
+ //Constructors
+ public Dog() {
+	 
+	 name = "null";
+	 age = 0;
+	 
+ }//end empty-argument constructor
+ 
+ public Dog(String name, int age) {
+	this.name = name;
+	this.age = age;
+}//end-preferred constructor
+
+ public String bark() {
+	 
+	 return "Bark, bark, bark!!!";
+ 
+ }//end bark
+ 
+ //Getter/Setters/ToString
+
+ public String getName() {
+	return name;
+}//end getName
+
+ public void setName(String name) {
+	this.name = name;
+}//end setName
+
+ public int getAge() {
+	return age;
+}//end getAge
+
+ public void setAge(int age) {
+	this.age = age;
+}//end setAge
+
+
+@Override
+public String toString() {
+	return "Dog [name=" + name + ", age=" + age + "]";
+}//end toString
+ 
+ 
+ 
+ 
+ 
+ 
+
+}//end class
